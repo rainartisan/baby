bd24f15457811b1fd8127a348d4aae5e39083c7c
rainartisan <apple@appledeMacBook-Pro.local>
2015-03-16 17:44:55 +0800

    just test

---
+++ justtest/justtest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  justtest
+//
+//  Created by rain on 15-3-16.
+//  Copyright (c) 2015年 ply365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
