5087dda815fd48d035c3a6749dd49757ac750e6e
rain <rainartisan@sohu.com>
2015-05-15 13:07:00 +0800

    15-mac-2222

--- justtest/justtest/AppDelegate.m
+++ justtest/justtest/AppDelegate.m
@@ -33,6 +33,8 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    //15-mac-2222
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
