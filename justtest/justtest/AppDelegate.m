6c678e2f42006a01937c70711b220b50382bcc83
rain <rainartisan@sohu.com>
2015-05-15 13:05:03 +0800

    Revert "15-mac-am"
    This reverts commit 06f2bb1efbd02fb9d18d32495c654c95c7edc7d3.

--- justtest/justtest/AppDelegate.m
+++ justtest/justtest/AppDelegate.m
@@ -38,7 +38,6 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    //15-mac-am-
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
